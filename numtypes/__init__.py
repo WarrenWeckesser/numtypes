211528fa4053c5af6c45e2d230750049ceba3af9
Warren Weckesser <warren.weckesser@gmail.com>
2019-09-11 11:14:30 -0400

    Add the code.

---
+++ numtypes/__init__.py
@@ -0,0 +1,1 @@
+from ._nint import nint32
---
+++ setup.py
@@ -0,0 +1,20 @@
+from os.path import join
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration(None, parent_package, top_path)
+    config.add_subpackage('numtypes')
+    config.add_extension('numtypes._nint',
+                    	 sources=[join('src', '_nint.c.src')])
+
+    return config
+
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+
+    setup(name='numtypes',
+    	  version='0.0.1',
+    	  configuration=configuration)
