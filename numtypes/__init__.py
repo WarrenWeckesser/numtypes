b5c8ac8416421ef48b6046ab2a8f98e18f5c5736
Warren Weckesser <warren.weckesser@gmail.com>
2022-05-29 14:25:04 -0400

    Trivial whitespace addition.

--- numtypes/__init__.py
+++ numtypes/__init__.py
@@ -3,7 +3,9 @@ from ._nint import nint32
 from ._complex_int import (complex_int8, complex_int16,
                            complex_int32, complex_int64)
 from ._polarcomplex import polarcomplex64, polarcomplex128
+
 # logfloat is a Python-only type.  It is not connected to NumPy
 from ._python_logtypes import logfloat
+
 # logfloat32 and logfloat64 are NumPy data types.
 from ._logtypes import logfloat32, logfloat64
